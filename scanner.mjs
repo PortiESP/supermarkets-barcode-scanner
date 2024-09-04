3c0523bd9b15c0d52712a7d5914c78c1df833b05
PortiESP <portiesp@gmail.com>
2024-09-04 19:41:26 +0200

    commented

--- scanner.mjs
+++ scanner.mjs
@@ -3,6 +3,20 @@ import { queryBarcode } from './supermarkets-barcodes-api-proxy/modules/proxy'
 
 let lastDetectionTimestamp = 0
 
+
+/**
+ * Setup the barcode scanner
+ * 
+ * @param {Object} options The options for the scanner
+ * @param {String} options.container The container where the scanner will be placed
+ * @param {Function} options.errorCallback The callback to be executed when an error occurs
+ * @param {Function} options.readyCallback The callback to be executed when the scanner is ready
+ * @param {Function} options.detectionCallback The callback to be executed when a barcode is detected
+ * @param {Object} options.detectionOptions The options for the detection
+ * @param {Boolean} options.detectionOptions.fetchBarcode Whether if the barcode should be fetched from the API
+ * @param {Number} options.detectionOptions.debounce The debounce time in ms
+ * @returns The start and stop functions for the scanner
+ */
 export function setupScanner({ container, errorCallback, readyCallback, detectionCallback, detectionOptions }) {
     // Setup of the barcode scanner
     Quagga.init({
@@ -37,17 +51,20 @@ export function setupScanner({ container, errorCallback, readyCallback, detectio
         if (readyCallback) readyCallback()
     })
 
+    // Setup the detection callback
     const { fetchBarcode, debounce } = detectionOptions
     Quagga.onDetected(async data => {
         // Prevent multiple calls to the callback for the same code
-        if (debounce){
+        if (debounce) {
+            // Check debounce time
             const now = Date.now()
             if (now - lastDetectionTimestamp < debounce) return false  // Debounced
 
+            // If passed the debounce time, update the timestamp
             lastDetectionTimestamp = now
         }
 
-
+        // Fetch the barcode from the API (if enabled)
         let result = undefined
         if (fetchBarcode) result = await queryBarcode(data.codeResult.code, data.codeResult.format)
 
